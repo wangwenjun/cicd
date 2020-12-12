cda5d936365e4ba129a8ab6a355f02e2a63d166a
wangwenjun <532500648@qq.com>
2020-12-12 15:00:10 +0800

    add the chapter09 package

--- src/main/java/com/wangwenjun/cicd/chapter09/package-info.java
+++ src/main/java/com/wangwenjun/cicd/chapter09/package-info.java
@@ -1,5 +1,5 @@
 package com.wangwenjun.cicd.chapter09;
 /**
  * Note:
- * <b>This chapter is about Jenkins installation and setting, no source code at all.</b>
+ * <b>This chapter is about Jenkins installation, fundamental tutorial, setting etc. no source code at all.</b>
  */
